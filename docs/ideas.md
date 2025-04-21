d183011e24654743f2c8f73ea1505274d25d917e
Brandon Hernandez <bhernandez@appliedmfg.com>
2025-04-21 12:56:32 -0400

    first commit

---
+++ docs/ideas.md
@@ -0,0 +1,4 @@
+4/21/2025
+
+-Write to the stdout instead of just println!(), so you can store the results in a file.
+-Implement a --file parameter, too.
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::io;
+use std::io::Read;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+}
+
+fn main() {
+    let arg = Cli::parse();
+    let mut input = String::new();
+    
+    // Get piped input
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("[-] Failed to read input.");
+
+    // Find pattern in input
+    for line in input.lines() {
+        if line.contains(&arg.pattern) {
+            println!("{line}");
+        }
+    }
+}
